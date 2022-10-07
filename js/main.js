28d1c02ff302218ed3f950fc42ae58bb36514f81
jsh030507 <jsh030507@naver.com>
2022-10-07 16:19:19 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function(){
+  let $bnnNum=0;
+
+  $(".prev").click(function(){
+    if($bnnNum<=0) return false;
+    $bnnNum--;
+    const $book_w=$("#main_image").width();
+    $("#main_image ul").animate({left:-$book_w*$bnnNum},500,function(){
+        $("#book_roll img").attr("src","image/state_out.png");
+        $("#book_roll img").eq($bnnNum).attr("src","image/state_over.png");
+    });
+  });
+
+  $(".next").click(function(){
+    if($bnnNum>=3) return false;
+    $bnnNum++;
+    const $book_w=$("#main_image").width();
+    $("#main_image ul").animate({left:-$book_w*$bnnNum},500,function(){
+        $("#book_roll img").attr("src","image/state_out.png");
+        $("#book_roll img").eq($bnnNum).attr("src","image/state_over.png");
+    });
+  });
+
+  $("#book_roll li a").click(function(){
+    const strName = ($(this).parent().attr("id"));
+                    slideTarget(strName.substr(3,1));
+                    return false;
+  });
+
+  function slideTarget(n){
+    const pos = Number(n) * - 100 + '%';
+    $("#main_image ul").animate({left:pos},500,
+    function(){
+    $("#book_roll img").attr("src","image/state_out.png");
+    $("#book_roll img").eq(n).attr("src","image/state_over.png");
+    });
+  };
+
+
+
+});
